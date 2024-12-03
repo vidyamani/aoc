7efaad7599b5c6d54e2e847ab5f2126ab708de49
Vidya Mani <vidya.mani@shopify.com>
2024-12-03 10:44:28 -0500

    2024 - day 2 cleanup

--- 2024/ruby/day02.rb
+++ 2024/ruby/day02.rb
@@ -3,18 +3,7 @@ def safe_report_count(levels)
 
   levels.split("\n").each do |str|
     row = str.split(" ").map(&:to_i)
-    next unless (row == row.sort || row == row.sort.reverse)
-
-    safe_row = true
-
-    row.each_with_index do |level, index|
-      if row[index+1]
-        if !(level - row[index+1]).abs.between?(1, 3)
-          safe_row = false
-        end
-      end
-    end
-    safe_reports += 1 if safe_row
+    safe_reports += 1 if safe_row?(row)
   end
 
   safe_reports
@@ -25,11 +14,8 @@ def safe_report_count_with_dampener(levels)
 
   levels.split("\n").each do |str|
     row = str.split(" ").map(&:to_i)
-    # next unless (row == row.sort || row == row.sort.reverse)
-
     if safe_row_with_dampener?(row)
       safe_reports += 1
-      # p row
     end
   end
   safe_reports
